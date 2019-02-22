b4debb582fb7d7f5936c9e5331835c41d5aacc1a
Beau V.C. Bellamy <bellamy.beau@gmail.com>
2019-02-22 01:34:18 -0800

    add a helper script preconfigured for COT and ATAK Chat.

---
+++ src/udprelay.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+LOCALIP=$1
+REMOTEIP=$2
+
+if [ -z "$LOCALIP" ]
+then
+    echo "Error: LOCALIP not set!"
+    return 1
+fi
+
+if [ -z "$REMOTEIP" ]
+then
+    echo "Error:REMOTEIP not set!"
+    return 1
+fi
+
+
+#COT
+echo "Starting COT Relay..."
+./udprelay $LOCALIP 239.2.3.1 6969 10000 $REMOTEIP 10000 &
+
+#CHAT
+echo "Starting ATAK Chat Relay..."
+./udprelay $LOCALIP 224.10.10.1 17012 10001 $REMOTEIP 10001 &
+
+for i in 1 2 ; do
+    wait -n
+    echo "exit code $?"
+done
